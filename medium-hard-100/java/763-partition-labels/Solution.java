df17c2ccab9346ac682c30e131d4c645d384e8d2
Vani.D <vanid2930@gmail.com>
2026-09-17 18:53:14 +0530

    Add Java solution 763 - Partition Labels

---
+++ medium-hard-100/java/763-partition-labels/Solution.java
@@ -0,0 +1,2 @@
+import java.util.*;
+class Solution { public List<Integer> partitionLabels(String s){int[]l=new int[26];for(int i=0;i<s.length();i++)l[s.charAt(i)-'a']=i;List<Integer>r=new ArrayList<>();int end=0,st=0;for(int i=0;i<s.length();i++){end=Math.max(end,l[s.charAt(i)-'a']);if(i==end){r.add(i-st+1);st=i+1;}}return r;} }
