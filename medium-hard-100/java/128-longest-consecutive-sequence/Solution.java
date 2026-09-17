043131a40b2f2a9778a99f89c7b1667295f7908e
Vani.D <vanid2930@gmail.com>
2026-09-17 18:51:51 +0530

    Add Java solution 128 - Longest Consecutive Sequence

---
+++ medium-hard-100/java/128-longest-consecutive-sequence/Solution.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer>s=new HashSet<>(); for(int x:nums)s.add(x); int ans=0;
+        for(int x:s) if(!s.contains(x-1)){int y=x; while(s.contains(y+1))y++; ans=Math.max(ans,y-x+1);} return ans;
+    }
+}
