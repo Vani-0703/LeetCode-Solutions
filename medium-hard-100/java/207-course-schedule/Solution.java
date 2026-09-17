4f9840c247c731e3983d37e7527ca5812b4d3adf
Vani.D <vanid2930@gmail.com>
2026-09-17 18:52:00 +0530

    Add Java solution 207 - Course Schedule

---
+++ medium-hard-100/java/207-course-schedule/Solution.java
@@ -0,0 +1,4 @@
+import java.util.*;
+class Solution {
+    public boolean canFinish(int n,int[][]p){ List<Integer>[]g=new List[n]; int[]d=new int[n]; for(int i=0;i<n;i++)g[i]=new ArrayList<>(); for(int[]e:p){g[e[1]].add(e[0]);d[e[0]]++;} Queue<Integer>q=new ArrayDeque<>(); for(int i=0;i<n;i++)if(d[i]==0)q.add(i); int c=0; while(!q.isEmpty()){int u=q.poll();c++;for(int v:g[u])if(--d[v]==0)q.add(v);} return c==n; }
+}
